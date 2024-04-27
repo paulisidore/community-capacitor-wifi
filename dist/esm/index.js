0547aa27293e4cc78655ee6a859be8c20b0fc5d7
André <andrerds.souza@gmail.com>
2024-04-26 21:03:30 -0300

    add folder dist

---
+++ dist/esm/index.js
@@ -0,0 +1,9 @@
+import { registerPlugin } from '@capacitor/core';
+const Wifi = registerPlugin('Wifi', {
+    web: () => import('./web').then(m => new m.WifiWeb()),
+    electron: () => window.CapacitorCustomPlatform.plugins.Wifi,
+    android: () => window.CapacitorCustomPlatform.plugins.Wifi
+});
+export * from './definitions';
+export { Wifi };
+//# sourceMappingURL=index.js.map
