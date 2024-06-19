6cb44aab596d3abd0645a882bf3863a16c1a9133
paulisidore <paul_isidore@hotmail.com>
2024-06-19 05:05:18 +0000

    PlugIn Fonctionnel sur Capacitor 6
    +Android OK
    *iOS non testé
    -web Non OK

--- dist/esm/web.d.ts
+++ dist/esm/web.d.ts
@@ -23,4 +23,14 @@ export declare class WifiWeb extends WebPlugin implements WifiPlugin {
         ssid: string | null;
     }>;
     disconnect(): Promise<void>;
+    /**
+     * Get Local IP Address
+     *
+     * @returns Promise Object
+     *
+     * getLocalIP().then((ipAddr) => {
+     *    console.log(ipAddr); // 192.168.0.122
+     * });
+     */
+    getLocalIP(): Promise<string>;
 }
--- dist/plugin.cjs.js
+++ dist/plugin.cjs.js
@@ -18,7 +18,13 @@ class WifiWeb extends core.WebPlugin {
         });
     }
     async getIP() {
-        return { ip: null };
+        const reponse = await this.getLocalIP().then((ipRecus) => {
+            return { ip: ipRecus };
+        }, (err) => {
+            console.error("ERR getIp pour le Web: ", err);
+            return { ip: "" };
+        });
+        return reponse;
     }
     async getSSID() {
         return { ssid: null };
@@ -33,6 +39,62 @@ class WifiWeb extends core.WebPlugin {
     }
     async disconnect() {
         return;
+    }
+    /**
+     * Get Local IP Address
+     *
+     * @returns Promise Object
+     *
+     * getLocalIP().then((ipAddr) => {
+     *    console.log(ipAddr); // 192.168.0.122
+     * });
+     */
+    getLocalIP() {
+        return new Promise(function (resolve, reject) {
+            // NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
+            console.log("Recherche LocalIP avec JavaScript...");
+            const RTCPeerConnection = window.RTCPeerConnection; // window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+            if (!RTCPeerConnection) {
+                reject('Your browser does not support this API');
+            }
+            const rtc = new RTCPeerConnection({ iceServers: [] });
+            //const addrs = {};
+            //let addrs["0.0.0.0"] = false;
+            const grepSDP = (sdp) => {
+                //let hosts = [];
+                let finalIP = '';
+                sdp.split('\r\n').forEach(function (line) {
+                    if (~line.indexOf("a=candidate")) { // http://tools.ietf.org/html/rfc4566#section-5.13
+                        const parts = line.split(' '), // http://tools.ietf.org/html/rfc5245#section-15.1
+                        addr = parts[4], type = parts[7];
+                        console.log("IP_: ", addr);
+                        if (type === 'host') {
+                            finalIP = addr;
+                        }
+                    }
+                    else if (~line.indexOf("c=")) { // http://tools.ietf.org/html/rfc4566#section-5.7
+                        console.log("IP type2: JavaScript...", line);
+                        const parts = line.split(' '), addr = parts[2];
+                        finalIP = addr;
+                    }
+                });
+                return finalIP;
+            };
+            // if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+            //     rtc.createDataChannel('', {reliable:false});
+            // };
+            rtc.onicecandidate = function (evt) {
+                console.log("Execution en JavaScript...", evt);
+                // convert the candidate to SDP so we can run it through our general parser
+                // see https://twitter.com/lancestout/status/525796175425720320 for details
+                if (evt.candidate) {
+                    const addr = grepSDP("a=" + evt.candidate.candidate);
+                    resolve(addr);
+                }
+            };
+            rtc.createOffer();
+            //rtc.createOffer();
+        });
     }
 }
 
